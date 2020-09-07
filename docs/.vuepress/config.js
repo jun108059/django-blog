4e5456cd75a7e2b716a2197e4226dc39fecbc5e5
YoungJun_Park <youngjun108059@gmail.com>
2020-09-07 23:57:01 +0900

    [Update]🧱Config - Sidebar Add Session 3

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,7 +19,8 @@ module.exports = {
     themeConfig: {
       sidebar: [
         sidebar.getSidebarGroup('/Blogdjango/', '💙 Session 1', true),
-        sidebar.getSidebarGroup('/Blogdjango2/', '💛 Session 2', true)
+        sidebar.getSidebarGroup('/Blogdjango2/', '💛 Session 2', true),
+        sidebar.getSidebarGroup('/RestFramework1/', '💜 Session 3', true)
       ],
       nav: [
         { text: 'GitHub', link: 'https://github.com/jun108059/django-blog' },
