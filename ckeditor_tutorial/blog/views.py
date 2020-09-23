caf9cddca727f0466339a05a97080be24e0e6c44
YoungJun_Park <youngjun108059@gmail.com>
2020-09-24 01:53:03 +0900

    [Update]💡Session 4 - CKEditor Base Code

--- ckeditor_tutorial/blog/views.py
+++ ckeditor_tutorial/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def home(request):
+    posts = Post.objects
+    return render(request, 'home.html', {'posts':posts})
